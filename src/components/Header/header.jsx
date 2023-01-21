8d268387a620921a7d3fa1100d7188c42f45c063
Totenkopf <pysskuu88@icloud.com>
2023-01-21 15:17:29 +0300

    удалил не используемые компоненты

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import { Avatar, IconButton } from '@mui/material';
+import { Avatar } from '@mui/material';
 import './index.css';
 
 const Header = ({userMe, name, avatar}) =>{
